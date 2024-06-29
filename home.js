ea94919d246932718540e3bf0b48055871c7a6e1
Soumyadeep Mukherjee <109720296+Soumya2025@users.noreply.github.com>
2024-06-30 00:45:30 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,31 @@
+var swiper = new Swiper(".mySwiper", {
+  loop: true,
+  spaceBetween: 10,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+var swiper2 = new Swiper(".mySwiper2", {
+  loop: true,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper,
+  },
+});
+// section4
+var swiper1 = new Swiper(".mySwiper1", {
+  cssMode: true,
+  navigation: {
+    nextEl: ".swiper-button-next1",
+    prevEl: ".swiper-button-prev1",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  mousewheel: true,
+  keyboard: true,
+});
